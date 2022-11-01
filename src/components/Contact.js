12c46e03879572ce7a1d03b718da94dfdfb7e30f
Hash <frechero.facundo.n@gmail.com>
2022-10-31 21:26:26 -0300

    added footer and implemented functions on contact btn

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,36 @@
 import { useState, useRef } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 import contactImg from "../assets/img/contact-img.svg";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
 export const Contact = () => {
   const [buttonText, setButtonText] = useState("Send");
-  const [status, setStatus] = useState({});
 
   const form = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    emailjs.sendForm('email_service', 'template_ahwde3u', form.current, 'xZl9d70wEtmkJVPf-')
-    .then((result) => {
-        console.log(result.text);
-    }, (error) => {
-        console.log(error.text);
-    });
+
+    setButtonText("Sending...");
+
+    emailjs
+      .sendForm(
+        "email_service",
+        "template_ahwde3u",
+        form.current,
+        "xZl9d70wEtmkJVPf-"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setButtonText("Sended");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
     e.target.reset();
   };
 
@@ -50,51 +61,40 @@ export const Contact = () => {
                     <input
                       type="text"
                       placeholder="First Name"
-                      name="firstName" required
+                      name="firstName"
+                      required
                     />
                   </Col>
                   <Col size={12} sm={6} className="px-1">
                     <input
                       type="text"
                       placeholder="Last Name"
-                      name="lastName" required
+                      name="lastName"
+                      required
                     />
                   </Col>
                   <Col size={12} sm={6} className="px-1">
                     <input
                       type="email"
                       placeholder="Email Address"
-                      name="email" required
+                      name="email"
+                      required
                     />
                   </Col>
                   <Col size={12} sm={6} className="px-1">
-                    <input
-                      type="tel"
-                      placeholder="Phone Number"
-                      name="phone"
-                    />
+                    <input type="tel" placeholder="Phone Number" name="phone" />
                   </Col>
                   <Col size={12} className="px-1">
                     <textarea
                       rows="6"
                       placeholder="Message"
-                      name="message" required
+                      name="message"
+                      required
                     ></textarea>
                     <button type="submit">
                       <span>{buttonText}</span>
                     </button>
                   </Col>
-                  {status.message && (
-                    <Col>
-                      <p
-                        className={
-                          status.success === false ? "danger" : "success"
-                        }
-                      >
-                        {status.message}
-                      </p>
-                    </Col>
-                  )}
                 </Row>
               </form>
             </div>
