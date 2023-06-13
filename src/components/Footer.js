9d71ca1abbc13953c72891e3c9fbf9750c7fca34
facundoFrechero <frechero.facundo.n@gmail.com>
2023-06-13 15:26:29 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,9 +14,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/facundo-frechero/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.github.com/facundoFrechero"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/_kuestionate/"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Facundo Frechero Portfolio</p>
           </Col>
